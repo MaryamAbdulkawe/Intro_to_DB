f362e7a2eac70b7dd21ac1c321e79df9a53c95e3
MaryamAbdulkawe <YOUR_GITHUB_EMAIL>
2025-08-31 07:16:36 +0300

    task 6: insert three customers (exact strings: emails + double-spaced Ave.)

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,9 @@
 -- ALL SQL KEYWORDS IN UPPERCASE
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@gandtech.com', '124 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@gandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (3, 'Obed Ohemeng', 'oobeng@gandtech.com', '125 Happiness Ave.');
+VALUES (3, 'Obed Ohemeng', 'oobeng@gandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (4, 'Nemehiah Kamolu', 'nkamolu@gandtech.com', '126 Happiness Ave.');
+VALUES (4, 'Nemehiah Kamolu', 'nkamolu@gandtech.com', '126 Happiness  Ave.');
