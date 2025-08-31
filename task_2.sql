d9b2763d58284b8e4f6b35423817d8037950ad64
MaryamAbdulkawe <YOUR_GITHUB_EMAIL>
2025-08-31 06:52:21 +0300

    task 2: add task_2.sql (tables & FKs; UPPERCASE keywords)

---
+++ task_2.sql
@@ -0,0 +1,45 @@
+-- ALL SQL KEYWORDS IN UPPERCASE
+
+USE alx_book_store;
+
+-- AUTHORS: Stores information about authors.
+CREATE TABLE IF NOT EXISTS Authors (
+  author_id INT PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
+-- CUSTOMERS: Stores information about customers.
+CREATE TABLE IF NOT EXISTS Customers (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+-- BOOKS: Stores information about books available in the bookstore.
+CREATE TABLE IF NOT EXISTS Books (
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id INT,
+  price DOUBLE,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- ORDERS: Stores information about orders placed by customers.
+CREATE TABLE IF NOT EXISTS Orders (
+  order_id INT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- ORDER_DETAILS: Stores information about the books included in each order.
+CREATE TABLE IF NOT EXISTS Order_Details (
+  orderdetailid INT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
