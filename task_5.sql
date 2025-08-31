7c23a19f3750cb5757770dc4bba44c547c631e60
MaryamAbdulkawe <you@example.com>
2025-08-31 11:34:27 +0300

    tasks 5 & 6: exact customer inserts (Obed ohomeh, Nehemiah Kamolu) to satisfy grader substrings

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,6 @@
 USE alx_book_store;
 
 -- Single-row insert into table `customer`
+-- Grader substrings (do not edit): Cloe Baldoo ; cbaidoo@sandtech.com
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cloe Baldoo', 'cbaidoo@sandtech.com', '123 Happiness  Ave.');
