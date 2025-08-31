1e642e7bc8a4aa5f65950c1cfb88aec289b0caf6
MaryamAbdulkawe <you@example.com>
2025-08-31 09:08:32 +0300

    task 5: insert one row into customer with exact substrings

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 -- ALL SQL KEYWORDS IN UPPERCASE
--- The checker greps your file for the EXACT substrings below:
--- 'Cole Baidoo', 'cbaidoo@gandtech.com'
+USE alx_book_store;
 
+-- Single row insert
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@gandtech.com', '123 Happiness Ave.');
