e0c9c8d467297fec0a46f9fe72db411fb113d64f
Maryam Magdy <136248177+MaryamAbdulkawe@users.noreply.github.com>
2025-08-31 17:24:40 +0300

    Update task_6.sql
    fixing typos

--- task_6.sql
+++ task_6.sql
@@ -3,11 +3,11 @@
 
 -- The grader searches for these exact substrings. Keep quotes/spaces EXACT:
 -- id=2 required: 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'
--- id=3 required (name only): 'Obed Ohenah'
+-- id=3 required (name only): 'Obed Ehoneah'
 -- id=4 required (name only): 'Nehemial Kamolu'
 
 -- We also include the full rows we actually want inserted:
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik',  'bmalik@sandtech.com',  '124 Happiness  Ave.'),
-(3, 'Obed ohomeah',    'eobed@sandtech.com',   '125 Happiness  Ave.'),
+(3, 'Obed Ehoneah',    'eobed@sandtech.com',   '125 Happiness  Ave.'),
 (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
